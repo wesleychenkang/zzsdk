631b154b8b478a2c55f3fbcea32fc5df17c124d0
nxliao <xtulnx@126.com>
2013-05-26 16:30:06 +0800

    UPDATE: close debug

--- src/com/zz/sdk/util/DebugFlags.java
+++ src/com/zz/sdk/util/DebugFlags.java
@@ -1,10 +1,10 @@
 package com.zz.sdk.util;
 
 public class DebugFlags {
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	
 	/** 测试：让支付的取消替换为成功 */
-	public static final boolean DEBUG_PAY_CANCEL_AS_SUCCESS = true;
+	public static final boolean DEBUG_PAY_CANCEL_AS_SUCCESS = false;
 	
 	public static final String DEF_LOGIN_NAME="zzzzaaaa";
 
